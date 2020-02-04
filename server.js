2e4be43b3a2dcfe5c066e692fbcc0c48b398b4f8
mrudul <mrudul1203@gmail.com>
2020-02-04 17:40:38 +0530

    'second'

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const hbs = require('hbs');
 
+
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/',(req, res) => {
     res.send('hello there');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('connected to server');
 });
